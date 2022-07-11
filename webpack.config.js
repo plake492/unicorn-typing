284a89d1d47cef9d4a3599c62d7adace1813d04c
Patrick Lake <plake492@gmail.com>
2022-07-11 00:34:56 -0400

    Update styles

--- webpack.config.js
+++ webpack.config.js
@@ -50,6 +50,10 @@ module.exports = (_, { mode }) => {
           }
         },
         {
+          test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+          loader: 'url-loader'
+        },
+        {
           test: /\.s[ac]ss$/i,
           use: [
             {
